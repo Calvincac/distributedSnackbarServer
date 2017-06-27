308ec2d1a01dbb2ecf29fdb07ed78945d02f44fc
Calvin Custodio <calvincac@hotmail.com>
2017-06-27 18:30:27 -0300

    Adicionada classe que cria threads

--- src/distributedsnackbarserver/DistributedSnackbarServer.java
+++ src/distributedsnackbarserver/DistributedSnackbarServer.java
@@ -5,12 +5,10 @@
  */
 package distributedsnackbarserver;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+
 
 /**
  *
@@ -18,87 +16,23 @@ import java.util.ArrayList;
  */
 public class DistributedSnackbarServer {
     
-    private static String choice = null;
-    private static User authenticatedUser;
+
 
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException, CloneNotSupportedException {
-        ServerSocket serverSocket = null;
-        Socket listenSocket = null;
-   
+        ServerSocket serverSocket;
+        serverSocket = null;
+        Socket listenSocket = null;   
 
         serverSocket = new ServerSocket(6800);
         System.out.println("Waiting for connection...");
         listenSocket = serverSocket.accept();
         System.out.println("Client connected!!");
-        DataInputStream input = new DataInputStream(listenSocket.getInputStream());
-        DataOutputStream output = new DataOutputStream(listenSocket.getOutputStream());
-             
-        String incomingRegistration  = input.readUTF();
-        log(incomingRegistration);
         
-        String incomingPassword  = input.readUTF();
-        log(incomingPassword);
-        
-        User newUser = new User(incomingRegistration, incomingPassword);
-        authenticateUser(newUser);
-        String message = showMenu();
-        output.writeUTF(message);
-        
-        while(choice != "exit") {
-            choice = input.readUTF();
-            float option = Float.parseFloat(choice);
-            String response = checkMoney(option);
-            output.writeUTF(response);
-            System.out.println("Loopiiiing");
-        }      
-    }
-    
-    public static void authenticateUser(User user) throws CloneNotSupportedException
-    {
-        UsersBuilder users = new UsersBuilder();
-        ArrayList usersFromDb = users.getUsers();       
-        
-        for(int i=0; i<usersFromDb.size(); i++) {
-            User chosen = (User) usersFromDb.get(i);
-            if(
-                    chosen.getRegistration().equalsIgnoreCase(user.getRegistration()) && 
-                    chosen.getPassword().equalsIgnoreCase(user.getPassword())
-               ) {
-                authenticatedUser = (User) chosen.clone();
-                System.out.println("User authenticated!");
-                return;
-            }
-        }
-        System.out.println("Registration or password is wrong");
-    }
-    
-    public static void log(String message){
-        System.out.println("Server message: " + message);
-    }
-    
-    public static void sendingLog(String message){
-        System.out.println("Sending message: " + message);
-    }
-    
-    public static String showMenu() {
-        String entrance = "What is the price of your dish:\n";
-        String menu = 
-                "Fish with carrots (R$: 3.00) "
-                + "\nPasta with potatoes (R$: 5.00) "
-                + "\nRice and beans (R$: 6.00)";
-        String result = entrance + menu;
-        return result;
-    }
-    
-    public static String checkMoney(float price) {
-        if(price > authenticatedUser.getMoney()) {
-            return "You don't have enough money to buy it";
-        }
-        authenticatedUser.discountMoney(price);
-        return "You will have it soon.";
-    }
+        new UsersThread(listenSocket).start();
+    }    
+
 }
---
+++ src/distributedsnackbarserver/UsersThread.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributedsnackbarserver;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author calvin
+ */
+public class UsersThread extends Thread {
+    
+    private String choice = null;
+    private User authenticatedUser;
+    private Socket listenSocket = null;
+
+    public UsersThread(Socket listenSocket) {
+        this.listenSocket = listenSocket;
+    }
+
+    @Override
+     public void run() {
+         
+        try {
+            DataInputStream input = new DataInputStream(listenSocket.getInputStream());
+            DataOutputStream output = new DataOutputStream(listenSocket.getOutputStream());
+            
+            String incomingRegistration  = input.readUTF();
+            log(incomingRegistration);
+            
+            String incomingPassword  = input.readUTF();
+            log(incomingPassword);
+            
+            User newUser = new User(incomingRegistration, incomingPassword);
+            authenticateUser(newUser);
+            String message = showMenu();
+            output.writeUTF(message);
+            
+            while(choice != "exit") {
+                choice = input.readUTF();
+                float option = Float.parseFloat(choice);
+                String response = checkMoney(option);
+                output.writeUTF(response);
+            }
+            
+            
+            
+        } catch (IOException | CloneNotSupportedException ex) {
+            Logger.getLogger(UsersThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+     }
+     
+     public void authenticateUser(User user) throws CloneNotSupportedException {
+        UsersBuilder users = new UsersBuilder();
+        ArrayList usersFromDb = users.getUsers();       
+        
+        for(int i=0; i<usersFromDb.size(); i++) {
+            User chosen = (User) usersFromDb.get(i);
+            if(
+                    chosen.getRegistration().equalsIgnoreCase(user.getRegistration()) && 
+                    chosen.getPassword().equalsIgnoreCase(user.getPassword())
+               ) {
+                authenticatedUser = (User) chosen.clone();
+                System.out.println("User authenticated!");
+                return;
+            }
+        }
+        System.out.println("Registration or password is wrong");
+     }
+     
+    public void log(String message) {
+        System.out.println("Server message: " + message);
+    }
+    
+    public void sendingLog(String message) {
+        System.out.println("Sending message: " + message);
+    }
+    
+    public String showMenu() {
+        String entrance = "What is the price of your dish:\n";
+        String menu = 
+                "Fish with carrots (R$: 3.00) "
+                + "\nPasta with potatoes (R$: 5.00) "
+                + "\nRice and beans (R$: 6.00)";
+        String result = entrance + menu;
+        return result;
+    }
+    
+    public String checkMoney(float price) {
+        if(price > authenticatedUser.getMoney()) {
+            return "You don't have enough money to buy it";
+        }
+        authenticatedUser.discountMoney(price);
+        return "You will have it soon.";
+    }
+}
